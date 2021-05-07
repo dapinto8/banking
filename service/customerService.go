e1495edda1d3040a57c53b3ce30ac00b94eee939
David Pinto <davidpinto999@gmail.com>
2021-05-06 22:28:10 -0400

    add customer dto

--- service/customerService.go
+++ service/customerService.go
@@ -2,24 +2,39 @@ package service
 
 import (
 	"github.com/dapinto8/banking/domain"
+	"github.com/dapinto8/banking/dto"
 	"github.com/dapinto8/banking/errs"
 )
 
 type CustomerService interface {
-	GetAllCustomer(status string) ([]domain.Customer, *errs.AppError)
-	GetCustomer(id string) (*domain.Customer, *errs.AppError)
+	GetAllCustomer(status string) ([]dto.CustomerResponse, *errs.AppError)
+	GetCustomer(id string) (*dto.CustomerResponse, *errs.AppError)
 }
 
 type DefaultCustomerService struct {
 	repository domain.CustomerRepository
 }
 
-func (s DefaultCustomerService) GetAllCustomer(status string) ([]domain.Customer, *errs.AppError) {
-	return s.repository.FindAll(status)
+func (s DefaultCustomerService) GetAllCustomer(status string) ([]dto.CustomerResponse, *errs.AppError) {
+	customers, err := s.repository.FindAll(status)
+	if err != nil {
+		return nil, err
+	}
+
+	response := make([]dto.CustomerResponse, 0)
+	for _, c := range customers {
+		response = append(response, c.ToDto())
+	}
+	return response, nil
 }
 
-func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer, *errs.AppError) {
-	return s.repository.ById(id)
+func (s DefaultCustomerService) GetCustomer(id string) (*dto.CustomerResponse, *errs.AppError) {
+	c, err := s.repository.ById(id)
+	if err != nil {
+		return nil, err
+	}
+	response := c.ToDto()
+	return &response, nil
 }
 
 func NewCustomerService(repository domain.CustomerRepository) DefaultCustomerService {
